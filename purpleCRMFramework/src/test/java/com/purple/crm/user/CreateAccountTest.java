32fc7019df00975983ce96589c18d368480306b9
admin <admin@DESKTOP-5NQK6OH>
2024-07-25 12:24:54 +0530

    commit message

---
+++ purpleCRMFramework/src/test/java/com/purple/crm/user/CreateAccountTest.java
@@ -0,0 +1,14 @@
+package com.purple.crm.user;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateAccountTest {
+@Test
+public void createAccountTest()
+{
+	Reporter.log("navigate to my account");
+	Reporter.log("login to applicate");
+	Reporter.log("add product");
+}
+}
