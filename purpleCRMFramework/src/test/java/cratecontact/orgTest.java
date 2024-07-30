be257f1417b34a42aa4a35786f41f52e9b0c1886
admin <admin@DESKTOP-5NQK6OH>
2024-07-30 16:25:46 +0530

    h

--- purpleCRMFramework/src/test/java/cratecontact/orgTest.java
+++ purpleCRMFramework/src/test/java/cratecontact/orgTest.java
@@ -6,6 +6,17 @@ public class orgTest {
 @Test
 public void loginTest()
 {
+	
+	String URL= System.getProperty("url");
+	String Browser= System.getProperty("browser");		
+	String UserName= System.getProperty("username");		
+	String Password= System.getProperty("password");		
+	System.out.println(URL);
+	System.out.println(Browser);
+	System.out.println(UserName);
+	System.out.println(Password);
+	
+	
 	System.out.println("hii");
 }
 @Test
